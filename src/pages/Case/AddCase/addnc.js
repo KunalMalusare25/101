fa2f5daf0ae58d03e79fc5097509d0896db57c0f
KunalMalusare25 <kunalmalusare2505@gmail.com>
2024-08-05 16:28:57 +0530

    firstgghb

--- src/pages/Case/AddCase/addnc.js
+++ src/pages/Case/AddCase/addnc.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./addnc.css";
 import JaminInfo from "./jamin";
+import KarjadaarInfo from "./karjadaarInfo";
 
 const Addnc = () => {
   const [activeTab, setActiveTab] = useState("thakbakidar");
@@ -10,34 +11,36 @@ const Addnc = () => {
   };
 
   return (
-    <div className="addnc-container">
-      <div className="tabs">
+    <div className="addnc-container-nc">
+      <div className="tabs-nc">
         <button
-          className={`tablinks ${activeTab === "thakbakidar" ? "active" : ""}`}
+          className={`tablinks-nc ${
+            activeTab === "thakbakidar" ? "active-nc" : ""
+          }`}
           onClick={() => handleTabClick("thakbakidar")}
         >
           थकबाकीदार माहिती
         </button>
         <button
-          className={`tablinks ${activeTab === "jamin" ? "active" : ""}`}
+          className={`tablinks-nc ${activeTab === "jamin" ? "active-nc" : ""}`}
           onClick={() => handleTabClick("jamin")}
         >
           जामीनदार माहिती
         </button>
         <button
-          className={`tablinks ${activeTab === "karja" ? "active" : ""}`}
+          className={`tablinks-nc ${activeTab === "karja" ? "active-nc" : ""}`}
           onClick={() => handleTabClick("karja")}
         >
           कर्जाची माहिती
         </button>
       </div>
 
-      <div className="tab-content">
+      <div className="tab-content-nc">
         {activeTab === "thakbakidar" && (
-          <div className="tabcontent active">
+          <div className="tabcontent-nc active-nc">
             <h3>थकबाकीदार माहिती</h3>
             <form>
-              <div className="form-row">
+              <div className="form-row-nc">
                 <label>नोंदणी क्र.</label>
                 <input
                   type="text"
@@ -45,7 +48,7 @@ const Addnc = () => {
                   readOnly
                 />
               </div>
-              <div className="form-row">
+              <div className="form-row-nc">
                 <label>ब्रँच चे नाव</label>
                 <select>
                   <option>Select</option>
@@ -57,40 +60,40 @@ const Addnc = () => {
                 <input type="text" value="देशपांडे विवेक" readOnly />
               </div>
 
-              <div className="form-row">
+              <div className="form-row-nc">
                 <label>
                   सुनावणीस हजर राहण्यासाठी अधिकार असलेली प्राधिकृत व्यक्ती
                 </label>
-                <div className="radio">
+                <div className="radio-nc">
                   <div>
                     <input type="radio" name="authority" value="manjur" /> मंजूर
                   </div>
-                  <div style={{ marginLeft: "10px" }}>
+                  <div style={{ marginLeft: "20px" }}>
                     <input type="radio" name="authority" value="adhikari" />
                     अधिकारी
                   </div>
                 </div>
               </div>
-              <div className="form-row">
+              <div className="form-row-nc">
                 <label>वकिलाचे नाव</label>
                 <select>
                   <option>Select</option>
                   {/* Add other options */}
                 </select>
               </div>
-              <div className="form-section">
+              <div className="form-section-nc">
                 <h3 style={{ marginTop: "10px" }}>थकबाकीदार</h3>
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>नाव *</label>
                   <input type="text" required />
                 </div>
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>पत्ता *</label>
                   <input type="text" required />
                   <label style={{ marginLeft: "100px" }}>वय</label>
                   <input type="number" value="0" min="0" />
                 </div>
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>जिल्हा *</label>
                   <select required>
                     <option>Select</option>
@@ -102,47 +105,41 @@ const Addnc = () => {
                     {/* Add other options */}
                   </select>
                 </div>
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>गाव *</label>
                   <input type="text" required />
                   <label style={{ marginLeft: "100px" }}>पिन</label>
                   <input type="text" />
                 </div>
 
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>मोबाईल नं.</label>
                   <input type="text" />
                   <label style={{ marginLeft: "100px" }}>ई-मेल</label>
                   <input type="email" />
                 </div>
 
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>CIN नं.</label>
                   <input type="text" />
                   <label style={{ marginLeft: "100px" }}>Account No</label>
                   <input type="email" />
                 </div>
 
-                <div className="form-row">
+                <div className="form-row-nc">
                   <label>व्यवसाय *</label>
                   <input type="text" required />
                 </div>
               </div>
               <hr />
-              <button type="submit" className="submit-btn">
+              <button type="submit" className="submit-btn-nc">
                 जतन करा
               </button>
             </form>
           </div>
         )}
-        {activeTab === "jamin" && <JaminInfo />}{" "}
-        {/* Include the new JaminInfo component */}
-        {activeTab === "karja" && (
-          <div className="tabcontent active">
-            <h3>कर्जाची माहिती</h3>
-            {/* Content for कर्जाची माहिती */}
-          </div>
-        )}
+        {activeTab === "jamin" ? <JaminInfo /> : null}
+        {activeTab === "karja" ? <KarjadaarInfo /> : null}
       </div>
     </div>
   );
