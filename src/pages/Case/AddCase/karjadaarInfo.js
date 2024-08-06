a3882d907768a9535645a5dbcb0d946c3f3a8473
PratikShelar <dtroy2399@gmail.com>
2024-08-06 17:59:58 +0530

    karjadaar

--- src/pages/Case/AddCase/karjadaarInfo.js
+++ src/pages/Case/AddCase/karjadaarInfo.js
@@ -1,259 +1,243 @@
-import React from 'react';
-import {
-  Box,
-  Button,
-  Divider,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  VStack,
-} from '@chakra-ui/react';
-
+import React from "react";
+import "./karja.css";
 const KarjadaarInfo = () => {
   return (
-    <Box p={6} borderWidth="1px" borderRadius="lg" bg="white">
-      <form>
-        <VStack spacing={6} align="stretch">
-          {/* Section 1 */}
-          <Heading size="md">कर्ज माहिती</Heading>
-          <FormControl>
-            <FormLabel>कर्ज प्रकार</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दायित्वास ठेवे घेणे बाकी रकमेसचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज अर्ज दाखल केल्याचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज अर्ज मंजुरी संचालक मंडळाचा सभेचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज अर्ज मंजुरी ठराव क्रमांक</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज विस्तार केल्याचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज मुदत</FormLabel>
-            <Input type="number" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्जाची अंतिम मुदत</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          
-          <Divider />
+    <div>
+      <form className="loan-form">
+        <div className="form-section">
+          <div className="form-group">
+            <label>कर्ज प्रकार</label>
+            <input type="text" />
+          </div>
+          <div className="form-group">
+            <label>दायित्वास ठेवे घेणे बाकी रकमेसचा दिनांक</label>
+            <input type="date" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज अर्ज दाखल केल्याचा दिनांक</label>
+            <input type="date" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज अर्ज मंजुरी संचालक मंडळाचा सभेचा दिनांक</label>
+            <input type="date" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज अर्ज मंजुरी ठराव क्रमांक</label>
+            <input type="text" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज विस्तार केल्याचा दिनांक</label>
+            <input type="date" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज मुदत</label>
+            <input type="number" />
+          </div>
+          <div className="form-group">
+            <label>कर्जाची अंतिम मुदत</label>
+            <input type="date" />
+          </div>
+        </div>
 
-          {/* Section 2: आकारणी */}
-          <Heading size="md">आकारणी</Heading>
-          <FormControl>
-            <FormLabel>कर्जाचा व्याजदर</FormLabel>
-            <Input type="number" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्जाचे दंडव्याज</FormLabel>
-            <Input type="number" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>पूर्वव्याजदर</FormLabel>
-            <Input type="number" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्जाचा खाती क्रमांक</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्जाची खाता</FormLabel>
-            <Input type="number" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>
+        <div className="form-section">
+          <h3>आकारणी</h3>
+          <div className="form-group">
+            <label>कर्जाचा व्याजदर</label>
+            <input type="number" />
+          </div>
+          <div className="form-group">
+            <label>कर्जाचे दंडव्याज</label>
+            <input type="number" />
+          </div>
+          <div className="form-group">
+            <label>पूर्वव्याजदर</label>
+            <input type="number" />
+          </div>
+          <div className="form-group">
+            <label>कर्जाचा खाती क्रमांक</label>
+            <input type="text" />
+          </div>
+          <div className="form-group">
+            <label>कर्जाची खाता</label>
+            <input type="number" />
+          </div>
+          <div className="form-group">
+            <label>
               कर्जाच्या अंतिम मुदतीपूर्वी दायित्वास ठेवे घेणे बाकी रकमेसचा
               दिनांकपर्यंतची पूर्ण रक्कम
-            </FormLabel>
-            <Input type="number" />
-          </FormControl>
+            </label>
+            <input type="number" />
+          </div>
+        </div>
 
-          <Divider />
+        <div className="form-section">
+          <div className="form-group">
+            <label>संचालक मंडळाच्या सभेचे १०२ कारवाई मंजुरीचा दिनांक</label>
+            <input type="date" />
+          </div>
+          <div className="form-group">
+            <label>संचालक मंडळाच्या सभेचे १०२ कारवाई मंजुरी ठराव क्रमांक</label>
+            <input type="text" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज बगळिकरणाची बिसलेला रजिस्टर्ड नोटीसचा दिनांक</label>
+            <input type="date" />
+          </div>
+        </div>
+        <div className="form-section">
+          <div className="form-group">
+            <label>कर्ज प्रकार</label>
+            <input type="text" />
+          </div>
+          {/* ... other form groups ... */}
+          <div className="form-group">
+            <label>कर्ज बगळिकरणाची बिसलेला रजिस्टर्ड नोटीसचा दिनांक</label>
+            <input type="date" />
+          </div>
+        </div>
 
-          {/* Section 3 */}
-          <FormControl>
-            <FormLabel>संचालक मंडळाच्या सभेचे १०२ कारवाई मंजुरीचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>संचालक मंडळाच्या सभेचे १०२ कारवाई मंजुरी ठराव क्रमांक</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज बगळिकरणाची बिसलेला रजिस्टर्ड नोटीसचा दिनांक</FormLabel>
-            <Input type="date" />
-          </FormControl>
+        {/* New Sections */}
+        <div className="form-section">
+          <div className="form-group">
+            <label>कर्ज मंजूर रक्कम</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत आकारलेले पूर्ण व्याज</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>आकारलेले दंड व्याज (दंडव्याज)</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>कर्ज मंजूर रक्कम अवधारित</label>
+            <input type="text" />
+          </div>
+        </div>
 
-          <Divider />
+        <div className="form-section">
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत मूळत: झळोली वसूली</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत व्याजात झळोली वसूली</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत दंड व्याजात झळोली वसूली</label>
+            <input type="number" placeholder="₹" />
+          </div>
+        </div>
 
-          {/* Section 4 */}
-          <FormControl>
-            <FormLabel>कर्ज मंजूर रक्कम</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत आकारलेले पूर्ण व्याज</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>आकारलेले दंड व्याज (दंडव्याज)</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कर्ज मंजूर रक्कम अवधारित</FormLabel>
-            <Input type="text" />
-          </FormControl>
+        <div className="form-section">
+          <div className="form-group">
+            <label>(अ) दिनांक 31/03/2024 पर्यंत मूळराशी येणे बाकी</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>(ब) दिनांक 31/03/2024 पर्यंत व्याजाची येणे बाकी</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>(क) दिनांक 31/03/2024 पर्यंत दंड व्याजाची येणे बाकी</label>
+            <input type="number" placeholder="₹" />
+          </div>
+        </div>
 
-          <Divider />
+        <div className="form-section">
+          <div className="form-group">
+            <label>विमा रक्कम (असेटच्या)</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत एकूण नोटीस, पोस्टेज इ. खर्च</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>दिनांक 31/03/2024 पर्यंत रिसल्व केलेला खर्च</label>
+            <input type="number" placeholder="₹" />
+          </div>
+          <div className="form-group">
+            <label>(अ + ब + क + ड = ह) एकूण वसूली झळोली वसूली</label>
+            <input type="number" placeholder="₹" />
+          </div>
+        </div>
+        <div className="form-section">
+        <div className="form-group">
+          <label>अर्जदारास झालेला खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>अर्जाला लावलेल्या स्टँप</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>कोर्ट फी स्टँप रक्कम</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>एखुण झालेली स्टँपची रक्कम</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>चौकशी फी व खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>इतर खर्च पोस्टेज इत्यादी</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>एखुण खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+      </div>
 
-          {/* Section 5 */}
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत मूळत: झळोली वसूली</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत व्याजात झळोली वसूली</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत दंड व्याजात झळोली वसूली</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-
-          <Divider />
-
-          {/* Section 6 */}
-          <FormControl>
-            <FormLabel>(अ) दिनांक 31/03/2024 पर्यंत मूळराशी येणे बाकी</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>(ब) दिनांक 31/03/2024 पर्यंत व्याजाची येणे बाकी</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>(क) दिनांक 31/03/2024 पर्यंत दंड व्याजाची येणे बाकी</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-
-          <Divider />
-
-          {/* Section 7 */}
-          <FormControl>
-            <FormLabel>विमा रक्कम (असेटच्या)</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत एकूण नोटीस, पोस्टेज इ. खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>दिनांक 31/03/2024 पर्यंत रिसल्व केलेला खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>(अ + ब + क + ड = ह) एकूण वसूली झळोली वसूली</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-
-          <Divider />
-
-          {/* Section 8 */}
-          <FormControl>
-            <FormLabel>अर्जदारास झालेला खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>अर्जाला लावलेल्या स्टँप</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कोर्ट फी स्टँप रक्कम</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>एखुण झालेली स्टँपची रक्कम</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>चौकशी फी व खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>इतर खर्च पोस्टेज इत्यादी</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>एखुण खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-
-          <Divider />
-
-          {/* Section 9 */}
-          <FormControl>
-            <FormLabel>जामीनदारांनी धारणाचा खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>कारवाईची फी</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>अन्‍य खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>एखुण खर्च</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>अर्जाची रक्कम</FormLabel>
-            <Input type="number" placeholder="₹" />
-          </FormControl>
-
-          <Divider />
-
-          {/* Section 10: तारणाची माहिती */}
-          <Heading size="md">तारणाची माहिती</Heading>
-          <FormControl>
-            <FormLabel>पगारीची माहिती</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>जंगम मालमत्तेचे वर्णन</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>स्थावर मालमत्तेचे वर्णन</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>इतर मालमत्तेचे वर्णन</FormLabel>
-            <Input type="text" />
-          </FormControl>
-
-          {/* Submit Button */}
-          <Button colorScheme="teal" type="submit">
-            सबमिट करा
-          </Button>
-        </VStack>
+      <div className="form-section">
+        <div className="form-group">
+          <label>जामीनदारांनी धारणाचा खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>कारवाईची फी</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>अन्‍य खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>एखुण खर्च</label>
+          <input type="number" placeholder="₹" />
+        </div>
+        <div className="form-group">
+          <label>अर्जाची रक्कम</label>
+          <input type="number" placeholder="₹" />
+        </div>
+      </div>
+      <div className="form-section">
+        <h3>तारणाची माहिती</h3>
+        <div className="form-group">
+          <label>पगारीची माहिती</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label>जंगम मालमत्तेचे वर्णन</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label>स्थावर मालमत्तेचे वर्णन</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label>इतर मालमत्तेचे वर्णन</label>
+          <input type="text" />
+        </div>
+      </div>
       </form>
-    </Box>
+    </div>
   );
 };
 
